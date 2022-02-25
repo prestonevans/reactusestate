fbeea98382c757c3ea46b1aad3d9bb515292b29f
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-24 20:32:01 -0700

    my version of a random title generator

--- src/components/useState/ex1/UseStateEx1.tsx
+++ src/components/useState/ex1/UseStateEx1.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react"
+import { randomTitle } from '../../../data/data'
+
+
 export default function UseStateEx1() {
+    const [ title, setTitle ] = useState('useState error example')
+
+    function clickHanlder() {
+        const current = randomTitle.indexOf(title)
+        let randomQuote = Math.floor(Math.random()*randomTitle.length)
+        while (current === randomQuote) {
+            randomQuote = Math.floor(Math.random()*randomTitle.length)
+        }
+        setTitle(randomTitle[randomQuote])
+    }
+
     return (
-        <h2>useState error example</h2>
+        <>
+            <h2>{title}</h2>
+            <button onClick={clickHanlder}>Click Me</button>
+        </>
     )
 }
---
+++ src/data/data.ts
@@ -0,0 +1,6 @@
+export const randomTitle = [
+    'You got this',
+    'Never say never',
+    'Generic Title Here',
+    'Who Wants To Code?'
+]
