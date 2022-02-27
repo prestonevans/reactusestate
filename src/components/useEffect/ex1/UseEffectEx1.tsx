aca7adcc979c42d14e0fecfbd1c96879a6f053ca
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-27 15:37:37 -0700

    playing around with useEffect

--- src/components/useEffect/ex1/UseEffectEx1.tsx
+++ src/components/useEffect/ex1/UseEffectEx1.tsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react'
 import './UseEffectEx1.css'
 
 export default function UseEffectEx1() {
-    return null
+    const [title, setTitle ] = useState(' useEffect Basics')
+    const [ hello, setHello ] = useState('hello')
+    useEffect(()=> {
+        setTimeout(() => {
+            setTitle('I got this stuff');
+            console.log('yep')
+        }, 2000);
+    },[title])
+    useEffect(() => {
+        console.log(hello)
+    },[hello])
+
+    function helloHandler() {
+        setHello(`${Math.floor(Math.random()*2)}`)
+    }
+    console.log('Render component')
+    return (
+    <>
+        <h2>{title}</h2>
+        <button onClick={helloHandler}>Log Hello</button>
+    </>
+    )
 }
