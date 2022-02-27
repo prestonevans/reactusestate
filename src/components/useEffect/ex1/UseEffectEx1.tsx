d7a3a206e7297eff623dbc690a4943f9968e3e8f
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-27 16:25:19 -0700

    use effect and conditional rendering

--- src/components/useEffect/ex1/UseEffectEx1.tsx
+++ src/components/useEffect/ex1/UseEffectEx1.tsx
@@ -2,26 +2,18 @@ import { useEffect, useState } from 'react'
 import './UseEffectEx1.css'
 
 export default function UseEffectEx1() {
-    const [title, setTitle ] = useState(' useEffect Basics')
-    const [ hello, setHello ] = useState('hello')
-    useEffect(()=> {
-        setTimeout(() => {
-            setTitle('I got this stuff');
-            console.log('yep')
-        }, 2000);
-    },[title])
-    useEffect(() => {
-        console.log(hello)
-    },[hello])
+    const [ count, setCount ] = useState(0)
 
-    function helloHandler() {
-        setHello(`${Math.floor(Math.random()*2)}`)
+    function addHandler() {
+        setCount(count + 1)
     }
-    console.log('Render component')
+    useEffect(() => {
+        document.title = `You have ${count} message${count === 1 ? '' : 's'}`
+    }, [count])
     return (
     <>
-        <h2>{title}</h2>
-        <button onClick={helloHandler}>Log Hello</button>
+        <h2>{count}</h2>
+        <button onClick={addHandler}>Add</button>
     </>
     )
 }
