c162d8812a3688c46153f69c7d445a7a24a7a2a6
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-28 08:49:17 -0700

    more useEffect practice

--- src/components/useEffect/ex1/UseEffectEx1.tsx
+++ src/components/useEffect/ex1/UseEffectEx1.tsx
@@ -2,18 +2,40 @@ import { useEffect, useState } from 'react'
 import './UseEffectEx1.css'
 
 export default function UseEffectEx1() {
-    const [ count, setCount ] = useState(0)
+    // const [ count, setCount ] = useState(0)
 
-    function addHandler() {
-        setCount(count + 1)
+    // function addHandler() {
+    //     setCount(count + 1)
+    // }
+    // useEffect(() => {
+    //     document.title = `You have ${count} message${count === 1 ? '' : 's'}`
+    // }, [count])
+    // return (
+    // <>
+    //     <h2>{count}</h2>
+    //     <button onClick={addHandler}>Add</button>
+    // </>
+    // )
+    const [message, setMessage] = useState('You are the best!')
+    const [loading, setLoading] = useState(false)
+    useEffect(()=> {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000);
+    },[message])
+
+    function changeMessageHandler() {
+        if(message === 'You are the best!') {
+            setMessage('Questionable...')
+        }   else {
+            setMessage('You are the best!')
+        }
     }
-    useEffect(() => {
-        document.title = `You have ${count} message${count === 1 ? '' : 's'}`
-    }, [count])
     return (
-    <>
-        <h2>{count}</h2>
-        <button onClick={addHandler}>Add</button>
-    </>
+        <>
+            <h1>{message}</h1>
+            <button disabled={loading} onClick={changeMessageHandler}>Yes I am</button>
+        </>
     )
 }
