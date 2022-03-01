409905ded2dae090d897bd052dd2392229ab7f8a
Preston Evans <preston.evans3074@stu.mtec.edu>
2022-02-28 22:15:06 -0700

    use effect and an api

--- src/components/useEffect/ex1/UseEffectEx1.tsx
+++ src/components/useEffect/ex1/UseEffectEx1.tsx
@@ -1,6 +1,16 @@
 import { useEffect, useState } from 'react'
 import './UseEffectEx1.css'
 
+type jokeProps = {
+    category : 'string';
+    correct_answer: 'string';
+    difficulty: 'string';
+    incorrect_answers: 'string'[];
+    question:'string';
+    type:'string';
+
+}
+
 export default function UseEffectEx1() {
     // const [ count, setCount ] = useState(0)
 
@@ -16,29 +26,45 @@ export default function UseEffectEx1() {
     //     <button onClick={addHandler}>Add</button>
     // </>
     // )
-    const [message, setMessage] = useState('You are the best!')
-    const [loading, setLoading] = useState(false)
+    // const [message, setMessage] = useState('You are the best!')
+    // const [loading, setLoading] = useState(false)
 
 
-    function changeMessageHandler() {
-        if(message === 'You are the best!') {
-            setMessage('Questionable...')
-            setLoading(true)
-            setTimeout(() => {
-                setLoading(false)
-        }, 2000);
-        }   else {
-            setMessage('You are the best!')
-            setLoading(true)
-            setTimeout(() => {
-                setLoading(false)
-        }, 2000);
+    // function changeMessageHandler() {
+    //     if(message === 'You are the best!') {
+    //         setMessage('Questionable...')
+    //         setLoading(true)
+    //         setTimeout(() => {
+    //             setLoading(false)
+    //     }, 2000);
+    //     }   else {
+    //         setMessage('You are the best!')
+    //         setLoading(true)
+    //         setTimeout(() => {
+    //             setLoading(false)
+    //     }, 2000);
+    //     }
+    // }
+    const [jokes, setJokes] = useState([])
+
+    useEffect(() => {
+        async function getData() {
+            const data = await fetch('https://opentdb.com/api.php?amount=10')
+            const joke = await data.json()
+            setJokes(joke.results)
         }
-    }
+        getData()
+    },[])
     return (
         <>
-            <h1>{message}</h1>
-            <button disabled={loading} onClick={changeMessageHandler}>Yes I am</button>
+            <h1>Hello</h1>
+            {jokes.map((joke:jokeProps,i) => {
+                return (
+                    <p key={i}>{joke.incorrect_answers.map((wrong:string) => {
+                        return <span>{wrong}</span>
+                    })}</p>
+                )
+            })}
         </>
     )
 }
